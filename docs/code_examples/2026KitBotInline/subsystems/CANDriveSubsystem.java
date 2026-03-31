42a6fb9468b3c2fc6e28e36ecf85a533d7927621
Carl Stanton <carl.macy.stanton@protonmail.com>
2026-03-31 00:35:32 +0000

    Refactored to use Krackens.

--- docs/code_examples/2026KitBotInline/subsystems/CANDriveSubsystem.java
+++ docs/code_examples/2026KitBotInline/subsystems/CANDriveSubsystem.java
@@ -7,11 +7,11 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -21,58 +21,53 @@ import static frc.robot.Constants.DriveConstants.*;
 
 public class CANDriveSubsystem extends SubsystemBase {
   // --8<-- [start: motors]
-  private final SparkMax leftLeader;
-  private final SparkMax leftFollower;
-  private final SparkMax rightLeader;
-  private final SparkMax rightFollower;
+  private final TalonFX leftLeader;
+  private final TalonFX leftFollower;
+  private final TalonFX rightLeader;
+  private final TalonFX rightFollower;
 
   // --8<-- [end: motors]
   private final DifferentialDrive drive;
 
   // --8<-- [start: constructor]
-  
+
   public CANDriveSubsystem() {
-    // create brushed motors for drive
-    leftLeader = new SparkMax(LEFT_LEADER_ID, MotorType.kBrushed);
-    leftFollower = new SparkMax(LEFT_FOLLOWER_ID, MotorType.kBrushed);
-    rightLeader = new SparkMax(RIGHT_LEADER_ID, MotorType.kBrushed);
-    rightFollower = new SparkMax(RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+    // Create TalonFX motors for drive
+    leftLeader = new TalonFX(LEFT_LEADER_ID);
+    leftFollower = new TalonFX(LEFT_FOLLOWER_ID);
+    rightLeader = new TalonFX(RIGHT_LEADER_ID);
+    rightFollower = new TalonFX(RIGHT_FOLLOWER_ID);
 
     // set up differential drive class
     drive = new DifferentialDrive(leftLeader, rightLeader);
 
-    // Set can timeout. Because this project only sets parameters once on
-    // construction, the timeout can be long without blocking robot operation. Code
-    // which sets or gets parameters during operation may need a shorter timeout.
-    leftLeader.setCANTimeout(250);
-    rightLeader.setCANTimeout(250);
-    leftFollower.setCANTimeout(250);
-    rightFollower.setCANTimeout(250);
+    // Create the configuration to apply to motors. Current limits help prevent
+    // tripping breakers or damaging motors. TalonFX uses stator current limiting
+    // which is more effective for FOC motors than supply current limiting.
+    TalonFXConfiguration config = new TalonFXConfiguration();
 
-    // Create the configuration to apply to motors. Voltage compensation
-    // helps the robot perform more similarly on different
-    // battery voltages (at the cost of a little bit of top speed on a fully charged
-    // battery). The current limit helps prevent tripping
-    // breakers.
-    SparkMaxConfig config = new SparkMaxConfig();
-    config.voltageCompensation(12);
-    config.smartCurrentLimit(DRIVE_MOTOR_CURRENT_LIMIT);
+    // Configure current limits
+    var currentLimits = config.CurrentLimits;
+    currentLimits.StatorCurrentLimit = DRIVE_MOTOR_CURRENT_LIMIT;
+    currentLimits.StatorCurrentLimitEnable = true;
 
-    // Set configuration to follow each leader and then apply it to corresponding
-    // follower. Resetting in case a new controller is swapped
-    // in and persisting in case of a controller reset due to breaker trip
-    config.follow(leftLeader);
-    leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    config.follow(rightLeader);
-    rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Set neutral mode to brake for better control
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-    // Remove following, then apply config to right leader
-    config.disableFollowerMode();
-    rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    // Set config to inverted and then apply to left leader. Set Left side inverted
-    // so that postive values drive both sides forward
-    config.inverted(true);
-    leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Apply base configuration to right leader
+    rightLeader.getConfigurator().apply(config);
+
+    // Set left leader to inverted so that positive values drive both sides forward
+    config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    leftLeader.getConfigurator().apply(config);
+
+    // Configure followers using TalonFX's Follower control request
+    // Right follower follows right leader without inversion
+    rightFollower.setControl(new Follower(RIGHT_LEADER_ID, false));
+
+    // Left follower follows left leader without additional inversion
+    // (the left leader already handles the inversion)
+    leftFollower.setControl(new Follower(LEFT_LEADER_ID, false));
   }
 
   // --8<-- [end: constructor]
--- docs/code_examples/2026KitBotInline/subsystems/CANFuelSubsystem.java
+++ docs/code_examples/2026KitBotInline/subsystems/CANFuelSubsystem.java
@@ -4,11 +4,11 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkMax;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,14 +16,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.FuelConstants.*;
 
 public class CANFuelSubsystem extends SubsystemBase {
-  private final SparkMax feederRoller;
-  private final SparkMax intakeLauncherRoller;
+  private final TalonFX feederRoller;
+  private final TalonFX intakeLauncherRoller;
 
   /** Creates a new CANBallSubsystem. */
   public CANFuelSubsystem() {
-    // create brushed motors for each of the motors on the launcher mechanism
-    intakeLauncherRoller = new SparkMax(INTAKE_LAUNCHER_MOTOR_ID, MotorType.kBrushed);
-    feederRoller = new SparkMax(FEEDER_MOTOR_ID, MotorType.kBrushed);
+    // create TalonFX motors for each of the motors on the launcher mechanism
+    intakeLauncherRoller = new TalonFX(INTAKE_LAUNCHER_MOTOR_ID);
+    feederRoller = new TalonFX(FEEDER_MOTOR_ID);
 
     // put default values for various fuel operations onto the dashboard
     // all methods in this subsystem pull their values from the dashbaord to allow
@@ -37,55 +37,61 @@ public class CANFuelSubsystem extends SubsystemBase {
 
     // create the configuration for the feeder roller, set a current limit and apply
     // the config to the controller
-    SparkMaxConfig feederConfig = new SparkMaxConfig();
-    feederConfig.smartCurrentLimit(FEEDER_MOTOR_CURRENT_LIMIT);
-    feederRoller.configure(feederConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    TalonFXConfiguration feederConfig = new TalonFXConfiguration();
+    var feederCurrentLimits = feederConfig.CurrentLimits;
+    feederCurrentLimits.StatorCurrentLimit = FEEDER_MOTOR_CURRENT_LIMIT;
+    feederCurrentLimits.StatorCurrentLimitEnable = true;
+    feederConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    feederRoller.getConfigurator().apply(feederConfig);
 
     // create the configuration for the launcher roller, set a current limit, set
     // the motor to inverted so that positive values are used for both intaking and
     // launching, and apply the config to the controller
-    SparkMaxConfig launcherConfig = new SparkMaxConfig();
-    launcherConfig.inverted(true);
-    launcherConfig.smartCurrentLimit(LAUNCHER_MOTOR_CURRENT_LIMIT);
-    intakeLauncherRoller.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    TalonFXConfiguration launcherConfig = new TalonFXConfiguration();
+    var launcherCurrentLimits = launcherConfig.CurrentLimits;
+    launcherCurrentLimits.StatorCurrentLimit = LAUNCHER_MOTOR_CURRENT_LIMIT;
+    launcherCurrentLimits.StatorCurrentLimitEnable = true;
+    launcherConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    launcherConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    intakeLauncherRoller.getConfigurator().apply(launcherConfig);
   }
 
   // A method to set the rollers to values for intaking
   public void intake() {
-    feederRoller.setVoltage(SmartDashboard.getNumber("Intaking feeder roller value", INTAKING_FEEDER_VOLTAGE));
+    feederRoller.setControl(new VoltageOut(SmartDashboard.getNumber("Intaking feeder roller value", INTAKING_FEEDER_VOLTAGE)));
     intakeLauncherRoller
-        .setVoltage(SmartDashboard.getNumber("Intaking intake roller value", INTAKING_INTAKE_VOLTAGE));
+        .setControl(new VoltageOut(SmartDashboard.getNumber("Intaking intake roller value", INTAKING_INTAKE_VOLTAGE)));
   }
 
   // A method to set the rollers to values for ejecting fuel out the intake. Uses
   // the same values as intaking, but in the opposite direction.
   public void eject() {
     feederRoller
-        .setVoltage(-1 * SmartDashboard.getNumber("Intaking feeder roller value", INTAKING_FEEDER_VOLTAGE));
+        .setControl(new VoltageOut(-1 * SmartDashboard.getNumber("Intaking feeder roller value", INTAKING_FEEDER_VOLTAGE)));
     intakeLauncherRoller
-        .setVoltage(-1 * SmartDashboard.getNumber("Intaking launcher roller value", INTAKING_INTAKE_VOLTAGE));
+        .setControl(new VoltageOut(-1 * SmartDashboard.getNumber("Intaking launcher roller value", INTAKING_INTAKE_VOLTAGE)));
   }
 
   // A method to set the rollers to values for launching.
   public void launch() {
-    feederRoller.setVoltage(SmartDashboard.getNumber("Launching feeder roller value", LAUNCHING_FEEDER_VOLTAGE));
+    feederRoller.setControl(new VoltageOut(SmartDashboard.getNumber("Launching feeder roller value", LAUNCHING_FEEDER_VOLTAGE)));
     intakeLauncherRoller
-        .setVoltage(SmartDashboard.getNumber("Launching launcher roller value", LAUNCHING_LAUNCHER_VOLTAGE));
+        .setControl(new VoltageOut(SmartDashboard.getNumber("Launching launcher roller value", LAUNCHING_LAUNCHER_VOLTAGE)));
   }
 
   // A method to stop the rollers
   public void stop() {
-    feederRoller.set(0);
-    intakeLauncherRoller.set(0);
+    feederRoller.setControl(new VoltageOut(0));
+    intakeLauncherRoller.setControl(new VoltageOut(0));
   }
 
   // A method to spin up the launcher roller while spinning the feeder roller to
   // push Fuel away from the launcher
   public void spinUp() {
     feederRoller
-        .setVoltage(SmartDashboard.getNumber("Spin-up feeder roller value", SPIN_UP_FEEDER_VOLTAGE));
+        .setControl(new VoltageOut(SmartDashboard.getNumber("Spin-up feeder roller value", SPIN_UP_FEEDER_VOLTAGE)));
     intakeLauncherRoller
-        .setVoltage(SmartDashboard.getNumber("Launching launcher roller value", LAUNCHING_LAUNCHER_VOLTAGE));
+        .setControl(new VoltageOut(SmartDashboard.getNumber("Launching launcher roller value", LAUNCHING_LAUNCHER_VOLTAGE)));
   }
 
   // A command factory to turn the spinUp method into a command that requires this
